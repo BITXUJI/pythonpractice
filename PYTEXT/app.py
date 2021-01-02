e67d9e975701cd1433eeb408fae04f139445ac24
BITXUJI <bitxuji@gmail.com>
2021-01-03 00:20:19 +0800

    twilio api practice

---
+++ PYTEXT/app.py
@@ -0,0 +1,8 @@
+from twilio.rest import Client
+import config
+client = Client(config.account_sid, config.auth_token)
+call = client.messages.create(
+    to="...",
+    from_="...",
+    body="This is our first message,You are right"
+)
