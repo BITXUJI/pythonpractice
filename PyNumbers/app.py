8aa75007eeac8a012f8ad42f99d185ea199aedc7
BITXUJI <bitxuji@gmail.com>
2021-01-03 16:17:47 +0800

    numpy

---
+++ PyNumbers/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+# array = np.zeros((3, 4), dtype=int)
+# array = np.ones((3, 4), dtype=int)
+# array = np.full((3, 4), 5, dtype=int)
+# array = np.random.random((3, 4))
+
+# array = np.array([[1, 2, 3], [4, 5, 6]])
+# print(array)
+# print(type(array))
+# print(array.shape)
+# print(array)
+# print(array[0, 0])
+# print(array > 0.2)
+# print(array[array > 0.2])
+# print(np.sum(array))
+# print(np.floor(array))
+# print(np.round(array))
+# print(np.ceil(array))
+
+
+# first = np.array([1, 2, 3])
+# second = np.array([1, 2, 3])
+# print(first+second)
+
+# print(first+2)
+
+
+dimensions_inch = np.array([1, 2, 3])
+dimensions_cm = dimensions_inch*2.54
+print(dimensions_cm)
